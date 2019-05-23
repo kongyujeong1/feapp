c48bee4722b7e7efdf57ca6014748805c714795c
kongyujeong1 <ouuj77@gmail.com>
2019-05-23 21:22:20 +0900

    Add Navigation.js

---
+++ src/Navigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, NavLink } from 'react-router-dom';
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <ul className="navigation">
+            <li>
+                 <Link to="/">Home</Link>
+            </li>
+            <li>
+                 <NavLink to="/about" activeClassName="selected">
+                      About
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/weather" activeClassName="selected">
+                      Weather
+                </NavLink>
+            </li>
+        </ul>
+
+    )
+
+}
+
+export default Navigation;
